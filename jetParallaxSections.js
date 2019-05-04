dc81c82585828f4570abacdc9a7274f4282203a8
trekgit <earnme.com@gmail.com>
2019-05-04 19:50:51 +0800

    Add files via upload

---
+++ jetParallaxSections.js
@@ -0,0 +1,3 @@
+/* <![CDATA[ */
+var elementorFrontendConfig = {"isEditMode":"","is_rtl":"","breakpoints":{"xs":0,"sm":480,"md":768,"lg":1025,"xl":1440,"xxl":1600},"version":"2.3.5","urls":{"assets":"http:\/\/ld-wp.template-help.com\/wordpress_prod-21421\/v1\/wp-content\/plugins\/elementor\/assets\/"},"settings":{"page":[],"general":{"elementor_global_image_lightbox":"yes","elementor_enable_lightbox_in_editor":"yes"}},"post":{"id":6,"title":"Home","excerpt":""}};
+/* ]]> */
